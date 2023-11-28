1135a4bf30f310222231393ce838abb1d52b8e8c
Ari Dyckovsky <aridyckovsky@gmail.com>
2023-11-28 12:45:05 -0500

    update structure of src and handle nested imports in playground

--- src/gentopo/cli/__init__.py
+++ src/gentopo/cli/__init__.py
@@ -14,8 +14,9 @@ from gentopo.topology import generate
     context_settings={"help_option_names": ["-h", "--help"]},
     invoke_without_command=True,
 )
-@click.version_option(version=__version__, prog_name="words-tui")
+@click.version_option(version=__version__, prog_name="gentopo")
 # @click.option("--db", "-d", envvar="WORDS_TUI_DB", default=Path.home() / ".words-tui.db", help="Database file to use")
 def gentopo():
     # TODO
-    generate()
+    # generate()
+    print("Ran gentopo with CLI command")
--- src/gentopo/topology/__init__.py
+++ src/gentopo/topology/__init__.py
@@ -1,9 +1,3 @@
 # SPDX-FileCopyrightText: 2023-present Ari Dyckovsky <aridyckovsky@gmail.com>
 #
 # SPDX-License-Identifier: MIT
-import sys
-
-if __name__ == "__main__":
-    from gentopo.cli import gentopo
-
-    sys.exit(gentopo())
