9a7e9d26fe31850ef532ab9598b654e0e38df02a
Ari Dyckovsky <aridyckovsky@gmail.com>
2023-12-04 17:50:50 -0500

    Overhaul of structure and begin custom small worlds in a family registry

---
+++ src/gentopo/topology/families.py
@@ -0,0 +1,79 @@
+import networkx as nx
+import random
+from gentopo.utils.decorators import Registry
+
+
+# A registry for network families that can be generated with GenTopo
+families = Registry()
+
+
+@families.register
+def lattice(n, k):
+    if k == n:
+        return nx.complete_graph(n)
+
+    G = nx.Graph()
+    nodes = list(range(n))
+
+    for j in range(1, k // 2 + 1):
+        targets = nodes[j:] + nodes[0:j]
+        G.add_edges_from(zip(nodes, targets))
+
+    return G
+
+
+@families.register
+def small_world(n, k, p, rounds):
+    """
+    Generates a connected small world network with n nodes.
+    """
+    if k == n:
+        return nx.complete_graph(n)
+
+    # Start with a ring lattice with k-neighbors per node
+    G = families.lattice(n, k)
+    nodes = list(G.nodes)
+
+    for j in range(1, k // 2 + 1):
+        targets = nodes[j:] + nodes[0:j]
+        for u, v in zip(nodes, targets):
+            # skip if u has as many edges as possible rounds
+            if G.degree(u) >= rounds:
+                continue
+            if random.random() < p:
+                w = random.choice(nodes)
+                # enforce no self loops or multiple edges
+                while w == u or G.has_edge(u, w):
+                    w = random.choice(nodes)
+                # skip if 2 has as many edges as possible rounds
+                if G.degree(w) >= rounds:
+                    continue
+                else:
+                    G.remove_edge(u, v)
+                    G.add_edge(u, w)
+    return G
+
+
+@families.register
+def connected_small_world(n, k, p, rounds, tries=100):
+    for i in range(tries):
+        G = families.small_world(n, k, p, rounds)
+        if nx.is_connected(G):
+            break
+        raise nx.NetworkXError("Maximum number of tries exceeded")
+    return G
+
+
+"""
+    sufficient = False
+    while not sufficient:
+        graph = nx.connected_watts_strogatz_graph(n, k, p, tries=100)
+        omega = nx.smallworld.omega(graph)
+        print(f"Value of omega: {omega}")
+
+        if min(omega_range) <= omega <= max(omega_range):
+            print(f"Ideal topology found with omega between {omega_range}")
+            sufficient = True
+
+    return graph
+"""
---
+++ src/gentopo/utils/decorators.py
@@ -0,0 +1,14 @@
+"""
+Decorators for GenTopo methods
+"""
+import functools
+
+
+class Registry(object):
+    def __init__(self):
+        # TODO: Include reference to version of package?
+        pass
+
+    def register(self, method):
+        name = getattr(method, "__name__", str(method))
+        setattr(self, name, method)
